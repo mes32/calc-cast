2f67e5b6d4dd1a7fcb9fffdce664e6204a7dee3e
mes32 <stockman.mike@gmail.com>
2019-06-09 18:45:32 -0500

    Add a conditional to socket port setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,13 +5,18 @@ import './App.css';
 import CalculationList from '../CalculationList/CalculationList';
 import CalculatorForm from '../CalculatorForm/CalculatorForm';
 
+let endpoint = 'http://127.0.0.1:5000'
+if (process.env.PORT) {
+    endpoint = 'https://fast-castle-54694.herokuapp.com:' + process.env.PORT;
+}
+
 class App extends Component {
 
     constructor() {
         super();
         this.state = {
             response: 0,
-            endpoint: "http://127.0.0.1:5000"
+            endpoint: endpoint
         };
     }
 
@@ -20,7 +25,10 @@ class App extends Component {
         //Very simply connect to the socket
         const socket = socketIOClient(endpoint);
         //Listen for data on the "outgoing data" namespace and supply a callback for what to do when we get one. In this case, we set a state variable
-        socket.on("outgoing data", data => this.setState({ response: data.num }));
+        socket.on("outgoing data", data => {
+            this.setState({ response: data.num });
+            console.log(`data.num = ${data.num}`);
+        });
     }
 
     render() {
