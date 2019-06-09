99fabd00ba49dc09de7c47cbf88eb69f11256a91
mes32 <stockman.mike@gmail.com>
2019-06-09 17:58:34 -0500

    Add package socket.io-client

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
 // Configure body parser middleware
 app.use(bodyParser.json());
@@ -13,8 +18,24 @@ app.use('/api/calculation', calculationRouter);
 // Serve the static site files
 app.use(express.static('build'));
 
+
+//Setting up a socket with the namespace "connection" for new sockets
+io.on("connection", socket => {
+    console.log("New client connected");
+
+    //Here we listen on a new namespace called "incoming data"
+    socket.on("incoming data", (data) => {
+        //Here we broadcast it out to all other sockets EXCLUDING the socket which sent us the data
+        socket.broadcast.emit("outgoing data", { num: data });
+    });
+
+    //A special namespace "disconnect" for when a client disconnects
+    socket.on("disconnect", () => console.log("Client disconnected"));
+});
+
+
 // Start the server listening on PORT = 5000
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import socketIOClient from "socket.io-client";
 import './App.css';
 
 import CalculationList from '../CalculationList/CalculationList';
 import CalculatorForm from '../CalculatorForm/CalculatorForm';
 
-function App() {
-    return (
-        <div className="App">
-            <CalculatorForm />
-            <CalculationList />
-        </div>
-    );
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            response: 0,
+            endpoint: "http://127.0.0.1:5000"
+        };
+    }
+
+    componentDidMount() {
+        const { endpoint } = this.state;
+        //Very simply connect to the socket
+        const socket = socketIOClient(endpoint);
+        //Listen for data on the "outgoing data" namespace and supply a callback for what to do when we get one. In this case, we set a state variable
+        socket.on("outgoing data", data => this.setState({ response: data.num }));
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <CalculatorForm />
+                <CalculationList />
+            </div>
+        );
+    }
 }
 
 export default App;
