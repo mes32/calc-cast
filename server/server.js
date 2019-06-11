7cdd0983a984d6651d01bba9ecf5ba8a4859ad28
mes32 <stockman.mike@gmail.com>
2019-06-11 12:57:49 -0500

    Remove references to socket.io and socket.io-client in attempt to debug 'Application error' on heroku-side

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const socketIo = require('socket.io');
+
+
+// const socketIo = require('socket.io');
 
 const app = express();
 // const server = http.createServer(app);
@@ -14,37 +16,37 @@ const server = app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
 
-const io = socketIo(server);
+// const io = socketIo(server);
 
-let tempID = 0;
-const expressionList = [];
+// let tempID = 0;
+// const expressionList = [];
 
-//Setting up a socket with the namespace 'connection' for new sockets
-io.on('connect', socket => {
-    console.log('New client connected');
+// //Setting up a socket with the namespace 'connection' for new sockets
+// io.on('connect', socket => {
+//     console.log('New client connected');
 
-    socket.emit('list expressions', { expressionList: expressionList });
+//     socket.emit('list expressions', { expressionList: expressionList });
 
-    socket.on('submit expression', expr => {
-        console.log(expr);
+//     socket.on('submit expression', expr => {
+//         console.log(expr);
 
-        tempID += 1;
-        if (expr.operator === 'ADD') {
-            expr = { ...expr, id: tempID, value: Number(expr.arg1) + Number(expr.arg2) };
-        } else if (expr.operator === 'SUB') {
-            expr = { ...expr, id: tempID, value: Number(expr.arg1) - Number(expr.arg2) };
-        } else if (expr.operator === 'MUL') {
-            expr = { ...expr, id: tempID, value: Number(expr.arg1) * Number(expr.arg2) };
-        } else if (expr.operator === 'DIV') {
-            expr = { ...expr, id: tempID, value: Number(expr.arg1) / Number(expr.arg2) };
-        }
+//         tempID += 1;
+//         if (expr.operator === 'ADD') {
+//             expr = { ...expr, id: tempID, value: Number(expr.arg1) + Number(expr.arg2) };
+//         } else if (expr.operator === 'SUB') {
+//             expr = { ...expr, id: tempID, value: Number(expr.arg1) - Number(expr.arg2) };
+//         } else if (expr.operator === 'MUL') {
+//             expr = { ...expr, id: tempID, value: Number(expr.arg1) * Number(expr.arg2) };
+//         } else if (expr.operator === 'DIV') {
+//             expr = { ...expr, id: tempID, value: Number(expr.arg1) / Number(expr.arg2) };
+//         }
 
-        expressionList.push(expr);
-        socket.emit('list expressions', { expressionList: expressionList });
-    });
+//         expressionList.push(expr);
+//         socket.emit('list expressions', { expressionList: expressionList });
+//     });
 
-    //A special namespace "disconnect" for when a client disconnects
-    socket.on("disconnect", () => console.log("Client disconnected"));
-});
+//     //A special namespace "disconnect" for when a client disconnects
+//     socket.on("disconnect", () => console.log("Client disconnected"));
+// });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,27 @@
 import React, { Component } from 'react';
-import io from 'socket.io-client';
+// import io from 'socket.io-client';
 import './App.css';
 
 import CalculationList from '../CalculationList/CalculationList';
 import CalculatorForm from '../CalculatorForm/CalculatorForm';
 
-// let endpoint = 'https://fast-castle-54694.herokuapp.com';
-// let endpoint = 'http://localhost';
-
 class App extends Component {
     constructor() {
         super();
-        this.socket = io();
+        // this.socket = io();
         this.state = {
             timeString: '',
             expressionList: []
         };
 
-        this.socket.on('time', timeString => {
-            this.setState({ timeString: timeString + '' });
-        });
+        // this.socket.on('time', timeString => {
+        //     this.setState({ timeString: timeString + '' });
+        // });
 
-        this.socket.on('list expressions', response => {
-            console.log(`list expressions: ${response.expressionList}`);
-            this.setState({ ...this.state, expressionList: response.expressionList });
-        });
+        // this.socket.on('list expressions', response => {
+        //     console.log(`list expressions: ${response.expressionList}`);
+        //     this.setState({ ...this.state, expressionList: response.expressionList });
+        // });
     }
 
     submitExpression = (expr) => {
@@ -37,7 +34,7 @@ class App extends Component {
                 <h1>Calc Cast</h1>
                 <CalculatorForm submitExpression={this.submitExpression} />
                 <CalculationList list={this.state.expressionList} />
-                {this.state.timeString}
+                {/* {this.state.timeString} */}
             </div>
         );
     }
